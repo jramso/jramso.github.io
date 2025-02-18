6108a26ea22a5035c023580e08b101a9762e1041
jramso <josue.rsou2@gmail.com>
2025-02-17 23:49:13 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/jramso.github.io/', // Altere para o nome do repositório
   plugins: [vue()],
-  base: '/jramso.github.io/', // ⚠️ Ajuste conforme seu repositório
 })
