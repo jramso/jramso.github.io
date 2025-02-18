e40687ea600217d7427e44f0b63de492f530c1e4
jramso <josue.rsou2@gmail.com>
2025-02-17 23:58:13 -0300

    trocando para raiz

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/jramso.github.io/', // Altere para o nome do repositório
+  base: '/',
   plugins: [vue()],
 })
